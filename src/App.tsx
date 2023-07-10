9f8b5a9e69432ddeee9b12111da709d8d9b57c40
tiagocoreform <tiago.matos@coreform.co>
2023-07-10 17:03:36 +1000

    adding proxy

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   const makeRequests = () => {
     for (let index = 0; index < 10; index++) {
-      axios.post(`https://dh7aht0kba.execute-api.ap-southeast-2.amazonaws.com/dev?param1=${index}`, {}, {
+      axios.post(`/dev?param1=${index}`, {}, {
         headers: {
           Accept: '*/*',
         }
