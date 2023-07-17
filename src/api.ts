1c8531bdbde54bb8c392c479fe2151fd82aa42c2
tiagocoreform <tiago.matos@coreform.co>
2023-07-17 19:01:29 +1000

    using fetch in service worker

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,8 @@
-import axios from 'axios';
-
 export const makeRequest = async (data: any) => {
   try {
-    await axios.post(`/api`, data, {
-      timeout: 1000,
-      headers: {
-        Accept: '*/*',
-      },
+    await fetch(`/api`, {
+      method: 'POST',
+      body: JSON.stringify(data)
     });
   } catch (error) {
     console.log(error);
