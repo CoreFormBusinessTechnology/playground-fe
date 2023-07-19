87e4818dd8bbc7e0a263236cf727e7305ede1cf8
tiagocoreform <tiago.matos@coreform.co>
2023-07-19 12:51:08 +1000

    only using createProxyMiddleware for non production

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:3000',
-      changeOrigin: true,
-    })
-  );
-};
+console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
+if (process.env.NODE_ENV !== 'production') {
+  module.exports = function(app) {
+    app.use(
+      '/api',
+      createProxyMiddleware({
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      })
+    );
+  };
+}
