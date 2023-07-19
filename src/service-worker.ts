22917a0ca70e9ab44be375d33b308cd00d51d934
tiagocoreform <tiago.matos@coreform.co>
2023-07-19 11:52:42 +1000

    using workbox BackgroundSyncPlugin again with new changes

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -12,9 +12,10 @@ import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+import { NetworkOnly, StaleWhileRevalidate } from 'workbox-strategies';
 import { db } from './db';
 import { makeRequest } from './api';
+import { BackgroundSyncPlugin } from 'workbox-background-sync';
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -81,35 +82,52 @@ self.addEventListener('message', (event) => {
 
 // Any other custom service worker logic can go here.
 // BACKGTOUND SYNC - START v1
-// const bgSyncPlugin = new BackgroundSyncPlugin('my-queue', {onSync: (p) => alert('bla')});
+const backgroundSyncCallbackFn = (bgSyncParam: any) => {
+  console.log('[SW] Background syncing', bgSyncParam.queue.name);
+  db.orders
+    .where('status')
+    .equals('not_synced')
+    .toArray().then(function (orders) {
+      orders.map(order => {
+        makeRequest(order);
+        db.orders.update(order, {status: 'synced'})
+        return order;
+      })
+    })
+}
 
-// registerRoute(/.*\/api.*/, new NetworkOnly({ plugins: [bgSyncPlugin] }), 'GET');
+const bgSyncPlugin = new BackgroundSyncPlugin('not-synced-orders', {
+  maxRetentionTime: 24 * 60,
+  onSync: backgroundSyncCallbackFn
+});
+
+registerRoute(/.*\/api.*/, new NetworkOnly({ plugins: [bgSyncPlugin] }), 'POST');
 // BACKGTOUND SYNC - END v1
 
 // BACKGTOUND SYNC - START v2
-self.addEventListener('sync', function(event) {
-  console.log('[SW] Background syncing', event);
+// self.addEventListener('sync', function(event) {
+//   console.log('[SW] Background syncing', event);
 
-  //@ts-ignore
-  if (event.tag === 'not-synced-order-tag') {
-    console.log('[SW] Syncing new orders');
-    //@ts-ignore
-    event.waitUntil(
-      db.orders
-      .where('status')
-      .equals('not_synced')
-      .toArray().then(function (orders) {
-        orders.map(order => {
-          makeRequest(order);
-          db.orders.update(order, {status: 'synced'})
-        })
-      })
-      .then(function () {
-        self.registration.showNotification('Orders synced with server');
-      })
-    );
+//   //@ts-ignore
+//   if (event.tag === 'not-synced-order-tag') {
+//     console.log('[SW] Syncing new orders');
+//     //@ts-ignore
+//     event.waitUntil(
+//       db.orders
+//       .where('status')
+//       .equals('not_synced')
+//       .toArray().then(function (orders) {
+//         orders.map(order => {
+//           makeRequest(order);
+//           db.orders.update(order, {status: 'synced'})
+//         })
+//       })
+//       .then(function () {
+//         self.registration.showNotification('Orders synced with server');
+//       })
+//     );
 
-  }
-})
+//   }
+// })
 // BACKGTOUND SYNC - END v2
 
